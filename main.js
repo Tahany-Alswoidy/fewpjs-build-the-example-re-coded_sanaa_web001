16ab42865f15281d379adbecd6ddbda592f1bf9c
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-15 11:34:15 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 document.getElementById("modal").classList.add("hidden");
 EMPTY_HEART.addEventListener('click',(e)=>{
-  mimicServerCall().then().then().
+  mimicServerCall().then(res=>res.json()).then(data=>data).catch((error) => {
+    document.getElementById("modal").className="";
+    document.getElementById("modal").innerHTML=error;
+    setTimeout(document.getElementById("modal").classList.add("hidden"),5000);
+  });
 });
 
 
